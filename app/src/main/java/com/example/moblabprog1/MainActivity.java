183c75ce9d344ea670035ce2f2a4fb5c134066cb
MetroSk <tautvydas.markulis@gmail.com>
2023-10-09 23:52:17 +0300

    Final

--- app/src/main/java/com/example/moblabprog1/MainActivity.java
+++ app/src/main/java/com/example/moblabprog1/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.moblabprog1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtView;
+    private Button btnChangeColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtView = findViewById(R.id.txtview);
+        btnChangeColor = findViewById(R.id.BtnChangeClr);
+
+        // Set a click listener for the button
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Change the text color of the TextView to a random color
+                int color = getRandomColor();
+                txtView.setTextColor(color);
+            }
+        });
+    }
+
+    // Helper method to generate a random color
+    private int getRandomColor() {
+        int r = (int) (Math.random() * 256);
+        int g = (int) (Math.random() * 256);
+        int b = (int) (Math.random() * 256);
+        return Color.rgb(r, g, b);
     }
 }
